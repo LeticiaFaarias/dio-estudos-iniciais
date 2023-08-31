ccf48efdea256e500a7d0a0c8596778b3dad7b5a
Leticiafarias <leticiafariasarquiteta@gmail.com>
2023-08-31 17:11:03 -0300

    atualizando a classe operadores

--- tipos-variaveis/src/main/java/br/com/curso/java/basico/Operadores.java
+++ tipos-variaveis/src/main/java/br/com/curso/java/basico/Operadores.java
@@ -1,4 +1,47 @@
 package br.com.curso.java.basico;
 
 public class Operadores {
+    public static void main(String[] args) {
+        String nomeUm = "LETICIA";
+        String nomeDois = new String("LETICIA");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2) { //true
+            System.out.println( "a nossa condição é verdadeira");
+        }
+
+        System.out.println("numero Um é igual a numero Dois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero Um é diferente de numero Dois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero Um é maior que numero Dois? " + simNao);
+
+        // OPERADORES LÓGICOS && e || Ou
+        // Exemplos abaixo
+
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && ( 7 > 4 ) ){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+        System.out.println("fim");
+    }
 }
+
+
+
